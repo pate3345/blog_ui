92ea6ad303d194f6d8bf2ba5d6d5b7e82da7a132
pate3345 <pate3345@sheridancollege.ca>
2025-06-20 19:02:50 -0400

    updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import Link from 'next/link';
-import { Button } from "@/components/ui/but"
+import { Button } from "@/components/ui/button"
 import {
     Sheet,
     SheetContent,
